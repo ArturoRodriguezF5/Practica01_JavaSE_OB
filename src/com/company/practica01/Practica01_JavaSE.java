61130e40af7831f7cc8273a65372e286850c908b
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-03-24 18:19:39 -0600

    Cambios a práctica 1 de Java SE Open Bootcamp

---
+++ src/com/company/practica01/Practica01_JavaSE.java
@@ -0,0 +1,39 @@
+package com.company.practica01;
+
+public class Practica01_JavaSE {
+
+    /**
+     * Método main donde se van a declarar variables de diferentes tipos de datos y se van
+     * a imprimir.
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Tipos de datos númericos
+        byte var1 = 0;
+        short var2 = 1;
+        int var3 = 2;
+        long var4 = 3;
+
+        // Tipos de datos decimales
+        float var5 = 4.2f;
+        double var6 = 5.2d;
+
+        // Tipos de datos booleanos
+        boolean var7 = true;
+        boolean var8 = false;
+
+        // Tipos de datos cadena o texto
+        char var9 = 'A';
+        String var10 = "Arturo";
+
+        System.out.println("Tipos de dato númericos: ");
+        System.out.println("byte: " + var1 + " short: " + var2 + " int: " + var3 + " long:  " + var4);
+        System.out.println("Tipos de dato decimales: ");
+        System.out.println("float: " + var5 + " double: " + var6);
+        System.out.println("Tipos de dato booleanos: ");
+        System.out.println("boolean: " + var7 + " boolean: " + var8);
+        System.out.println("Tipos de dato cadena o texto: ");
+        System.out.println("char: " + var9 + " String: " + var10);
+
+    }
+}
